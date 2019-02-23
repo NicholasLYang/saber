4a1ea8d0521585e815bd9c81baebe5e75a88398c
Nicholas Yang <ny585@nyu.edu>
2019-02-22 21:28:36 -0500

    Added type sig parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,21 @@
-use crate::ast::{Expr, Pat, Stmt};
+use crate::ast::{Expr, Pat, Stmt, TypeSig};
 use crate::lexer::{Lexer, Token};
 use crate::types::Result;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 
 pub struct Parser<'input> {
     lexer: Lexer<'input>,
+    pushedback_tokens: Vec<(usize, Token, usize)>,
+}
+
+#[derive(Debug, PartialEq)]
+struct Location(usize, usize);
+
+impl Display for Location {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "({}---{})", self.0, self.1)
+    }
 }
 
 #[derive(Debug, Fail, PartialEq)]
@@ -11,25 +23,43 @@ pub enum ParseError {
     #[fail(display = "Reached end of file without completing parse")]
     EndOfFile,
     #[fail(
-        display = "Unexpected token {:?}, expected {:?}",
-        token, expected_tokens
+        display = "({}): Unexpected token {:?}, expected {:?}",
+        location, token, expected_tokens
     )]
     UnexpectedToken {
         token: Token,
         expected_tokens: Vec<&'static str>,
+        location: Location,
     },
     #[fail(display = "Not implemented yet")]
     NotImplemented,
+    #[fail(display = "Should not be reachable")]
+    NotReachable,
 }
 
 impl<'input> Parser<'input> {
     pub fn new(lexer: Lexer) -> Parser {
-        Parser { lexer }
+        Parser {
+            lexer,
+            pushedback_tokens: Vec::new(),
+        }
     }
+
+    fn pushback(&mut self, token: (usize, Token, usize)) {
+        self.pushedback_tokens.push(token);
+    }
+
     fn get_next_token(&mut self) -> Result<(usize, Token, usize)> {
-        match self.lexer.next() {
-            Some(tok) => Ok(tok?),
-            None => Err(ParseError::EndOfFile)?,
+        if !self.pushedback_tokens.is_empty() {
+            match self.pushedback_tokens.pop() {
+                Some(tok) => Ok(tok),
+                None => Err(ParseError::NotReachable)?,
+            }
+        } else {
+            match self.lexer.next() {
+                Some(tok) => Ok(tok?),
+                None => Err(ParseError::EndOfFile)?,
+            }
         }
     }
     pub fn parse_statement(&mut self) -> Result<Stmt> {
@@ -54,10 +84,47 @@ impl<'input> Parser<'input> {
         match tok {
             (_, Token::LParen, _) => Ok(Pat::Tuple(Vec::new())),
             (_, Token::LBrace, _) => Ok(Pat::Record(Vec::new())),
-            (_, Token::Ident(name), _) => Ok(Pat::Id(name, None)),
-            (_, token, _) => Err(ParseError::UnexpectedToken {
+            (_, Token::Ident(name), _) => Ok(Pat::Id(name, self.parse_type_sig()?)),
+            (start, token, end) => Err(ParseError::UnexpectedToken {
                 token,
+                location: Location(start, end),
                 expected_tokens: vec!["(", "{", "identifier"],
+            })?,
+        }
+    }
+
+    fn parse_type_sig(&mut self) -> Result<Option<TypeSig>> {
+        let token = self.get_next_token()?;
+        match token {
+            (_, Token::Colon, _) => Ok(Some(self.parse_type()?)),
+            token => {
+                self.pushback(token);
+                Ok(None)
+            }
+        }
+    }
+
+    fn parse_type(&mut self) -> Result<TypeSig> {
+        let tok = self.get_next_token()?;
+        match tok {
+            (_, Token::Ident(name), _) => Ok(TypeSig::Name(name)),
+            (_, Token::LBracket, _) => {
+                let array_type = self.parse_type()?;
+                let (start, bracket, end) = self.get_next_token()?;
+                if bracket != Token::RBracket {
+                    Err(ParseError::UnexpectedToken {
+                        token: bracket,
+                        location: Location(start, end),
+                        expected_tokens: vec!["]"],
+                    })?
+                } else {
+                    Ok(TypeSig::Array(Box::new(array_type)))
+                }
+            }
+            (start, token, end) => Err(ParseError::UnexpectedToken {
+                token,
+                location: Location(start, end),
+                expected_tokens: vec!["[", "identifier"],
             })?,
         }
     }
