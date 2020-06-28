d322f3c0e6c739228ca67c1f9127048b44dc882b
Nicholas Yang <ny585@nyu.edu>
2020-06-28 17:22:42 -0400

    Made sure to read entire length, not just first byte

--- build/load.js
+++ build/load.js
@@ -28,7 +28,10 @@ const printHeap = memArray => {
 }
 
 const printString = memArray => ptr => {
-    const len = memArray[ptr];
+    const len = memArray[ptr]
+        + (memArray[ptr + 1] << 8)
+        + (memArray[ptr + 2]<< 16)
+        + (memArray[ptr + 3] << 24);
     const out = new Uint8Array(len);
     for (let offset = 0; offset < len; offset++) {
         const i = offset + ptr + 4;
