a279be820dd2d07a6228e56cf2aa78900583a7a6
Nicholas Yang <ny585@nyu.edu>
2020-07-03 16:22:35 -0400

    Removed doc folder. Might make a return soon

--- doc/grammar.el
+++
@@ -1,9 +0,0 @@
-(setq grammar-highlights
-      '(("\'.*\'" . font-lock-string-face)
-	("[A-Z][A-Za-z]*" . font-lock-function-name-face)
-	("[a-z][A-Za-z_]*" . font-lock-constant-face)
-	))
-
-(define-derived-mode grammar-mode fundamental-mode "grammar"
-  "major mode for editing grammar files."
-  (setq font-lock-defaults '(grammar-highlights)))
